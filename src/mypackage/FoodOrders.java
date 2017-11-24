06462a6ab6290ca49943f7b05e0133aed5f71551
Karin Golan <Karingolan25@gmail.com>
2017-11-24 17:41:23 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- src/mypackage/FoodOrders.java
+++ src/mypackage/FoodOrders.java
@@ -20,16 +20,7 @@ public class FoodOrders {
 			
 		
 		//calling to Database jar to store the data
-			 Process proc1 = Runtime.getRuntime().exec( "java -jar C:\\FinalProject\\Database.jar "+total_cost);
-			 proc1.waitFor();
-			 InputStream in = proc1.getInputStream();
-			 InputStream err = proc1.getErrorStream();
-			   byte[] b=new byte[in.available()];
-			    in.read(b,0,b.length);
-			    System.out.println(new String(b));
-			    byte c[]=new byte[err.available()];
-			    err.read(c,0,c.length);
-			    System.out.println(new String(c));
+			
 			  
 		}
 		
