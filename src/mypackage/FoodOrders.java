fbe725102ba82ee4eb9cd100e0527cc5ff349f89
Karin Golan <Karingolan25@gmail.com>
2017-12-05 07:41:40 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- src/mypackage/FoodOrders.java
+++ src/mypackage/FoodOrders.java
@@ -1,30 +1,76 @@
 package mypackage;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class FoodOrders {
 
 
-		public static void main(String[] args  ) throws Exception, InterruptedException {
+		public static void main(String[] args) throws Exception, InterruptedException {
 			
+				ServerSocket listener = new ServerSocket(8080);
+		        try {
+		            while (true) {
+		                new CalculateTotalCost(listener.accept()).start();
+		            }
+		        } finally {
+		            listener.close();
+		        }
+
 		
-		double total_cost = Double.parseDouble (args[0]);	
-		//decler a VAT object
-		VAT Vatobject =new VAT();
 		
 		
 		//operate a metode 
-		total_cost=Vatobject.totalWithVat(total_cost);
+		//total_cost=Vatobject.totalWithVat(total_cost);
 		
 		//total_cost= maincourse+side+drink;
-			System.out.println("The total cost of the order is :" +total_cost);
-			
-		
-		//calling to Database jar to store the data
-			
-			  
+		//	System.out.println("The total cost of the order is :" +total_cost);
 		}
 		
-}		
+	    private static class CalculateTotalCost extends Thread {
+	        private Socket socket;
+			BufferedReader inFromDB;
+		    PrintWriter outToDB;
+		    double total_cost;
+	        public CalculateTotalCost(Socket socket) {
+	            this.socket = socket;
+	        }
+	        
+	        public void run() {
+	            try {
+
+	            	BufferedReader in = new BufferedReader(
+	                        new InputStreamReader(socket.getInputStream()));
+	                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	                while (true) {
+	                    String input = in.readLine();
+	                    if (input == null || input.equals(".")) {
+	                        break;
+	                    }
+	        	        
+	                    VAT Vatobject =new VAT();
+	                    total_cost = Vatobject.totalWithVat(Double.parseDouble(input));
+	                    out.println(total_cost);
+	                    
+	        		    Socket socketDB = new Socket("localhost",8090);
+	        	        inFromDB = new BufferedReader(
+	        	                new InputStreamReader(socketDB.getInputStream()));
+	        	        outToDB = new PrintWriter(socketDB.getOutputStream(), true);
+	        	        outToDB.println(total_cost);
+
+	            }
+	            }catch (IOException e) {
+	            }
+	        }       
+	    }
+	}
+		
+
+
 				
 				
 
