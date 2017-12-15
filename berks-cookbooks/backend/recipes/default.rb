68b2d28d34104e87b3cbdea861ee0c9395e3e8aa
Karin Golan <Karingolan25@gmail.com>
2017-12-15 15:07:52 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- berks-cookbooks/backend/recipes/default.rb
+++ berks-cookbooks/backend/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright (C) 2017 
 #
 execute "download file from  s3" do
-command "aws s3 cp s3://['BucketName']/BackEnd-0.0.1-SNAPSHOT.jar   /home/ec2-user/BackEnd-0.0.1-SNAPSHOT.jar"
+command "aws s3 cp s3://${BucketName}/BackEnd-0.0.1-SNAPSHOT.jar   /home/ec2-user/BackEnd-0.0.1-SNAPSHOT.jar"
  action "run"
 end
 
