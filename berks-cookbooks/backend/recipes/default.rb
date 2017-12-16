02323268f2539fe3ac65ecd9a2f9f4e4733d6120
Karin Golan <Karingolan25@gmail.com>
2017-12-16 18:51:25 +0100

    f

--- berks-cookbooks/backend/recipes/default.rb
+++ berks-cookbooks/backend/recipes/default.rb
@@ -4,13 +4,13 @@
 #
 # Copyright (C) 2017 
 #
-execute "download file from  s3" do
-command "aws s3 cp s3://backendbucket1/BackEnd-0.0.1-SNAPSHOT.jar   /home/ec2-user/BackEnd-0.0.1-SNAPSHOT.jar"
- action "run"
+execute "download file from s3" do
+ command "aws s3 cp s3://#{node['backend']['BucketName']}/BackEnd-0.0.1-SNAPSHOT.jar   /home/ec2-user/BackEnd-0.0.1-SNAPSHOT.jar"
+action "run"
 end
 
 execute "running BackEnd.jar" do
   command "java -jar /home/ec2-user/BackEnd-0.0.1-SNAPSHOT.jar "
   cwd "/home/ec2-user"
-  action "run"
+action "run"
 end
